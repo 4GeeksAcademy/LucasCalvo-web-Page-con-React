476dd95f0ab075e1cef52934dcffb8a189f451b2
Lucas Matias Calvo <lucascalvo2903@gmail.com>
2025-10-14 02:32:27 +0000

    entrego

--- src/js/components/Card.jsx
+++ src/js/components/Card.jsx
@@ -1,60 +1,21 @@
 import React from "react";
 
 
-const Card = () => {
+const Card = (props) => {
+
 	return (
     
-    <div className="container mt-5">
-      <div className="row justify-content-center">
-  {/* 1 */}
-        <div className="col-md-3">
           <div className="card" style={{ width: "18rem" }}>
-            <img src="https://fastly.picsum.photos/id/29/4000/2670.jpg?hmac=rCbRAl24FzrSzwlR5tL-Aqzyu5tX_PA95VJtnUXegGU" className="card-img-top" alt="..." />
+            <img src= {props.img} className="card-img-top" alt="..." />
             <div className="card-body text-center">
-              <h5 className="card-title">Carta</h5>
-              <p className="card-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. </p>
+              <h5 className="card-title">{props.titulo}</h5>
+              <p className="card-text">{props.descripcion}</p>
               <a href="#" className="btn btn-primary">Botón</a>
             </div>
           </div>
-        </div>
-
-      
-      {/* 2 */}
-        <div className="col-md-3">
-          <div className="card" style={{ width: "18rem" }}>
-            <img src="https://fastly.picsum.photos/id/16/2500/1667.jpg?hmac=uAkZwYc5phCRNFTrV_prJ_0rP0EdwJaZ4ctje2bY7aE" className="card-img-top" alt="..." />
-            <div className="card-body text-center">
-              <h5 className="card-title">Carta</h5>
-              <p className="card-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. </p>
-              <a href="#" className="btn btn-primary">Botón</a>
-            </div>
-          </div>
-        </div>
-
-        {/* 3*/}
-        <div className="col-md-3">
-          <div className="card" style={{ width: "18rem" }}>
-            <img src="https://fastly.picsum.photos/id/19/2500/1667.jpg?hmac=7epGozH4QjToGaBf_xb2HbFTXoV5o8n_cYzB7I4lt6g" className="card-img-top" alt="..." />
-            <div className="card-body text-center">
-              <h5 className="card-title">Carta</h5>
-              <p className="card-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. </p>
-              <a href="#" className="btn btn-primary">Botón</a>
-            </div>
-          </div>
-        </div>
-        {/* 4*/}
-        <div className="col-md-3">
-          <div className="card" style={{ width: "18rem" }}>
-            <img src="https://fastly.picsum.photos/id/1/5000/3333.jpg?hmac=Asv2DU3rA_5D1xSe22xZK47WEAN0wjWeFOhzd13ujW4" className="card-img-top" alt="..." />
-           <div className="card-body text-center">
-              <h5 className="card-title">Carta</h5>
-              <p className="card-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. </p>
-              <a href="#" className="btn btn-primary">Botón</a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+       
+     
+        
   );
 };
 
--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -9,23 +9,39 @@ import Jumbotron from "./Jumbotron.jsx"
 
 //create your first component
 const Home = () => {
-  return (
-<>
-      <div className="w-100">
-        <Navbar />
-      </div>
+  let paisajes = [
+    {
+      img: "https://fastly.picsum.photos/id/29/4000/2670.jpg?hmac=rCbRAl24FzrSzwlR5tL-Aqzyu5tX_PA95VJtnUXegGU", titulo: "Snow Mountain", descripcion: "Lorem Ipsum is simply dummy text of the printing and typesetting industry."},
+    { 
+      img: "https://fastly.picsum.photos/id/16/2500/1667.jpg?hmac=uAkZwYc5phCRNFTrV_prJ_0rP0EdwJaZ4ctje2bY7aE", titulo: "Sound Lake", descripcion: "Lorem Ipsum is simply dummy text of the printing and typesetting industry." },
 
-      <div className="container">
-		 <div className="bg-body-secondary p-4 mt-2 shadow-sm">
-        <Jumbotron />
-		 </div>
-		 </div>
-	<div className="container">
-        <Card />
+    { 
+      img: "https://fastly.picsum.photos/id/19/2500/1667.jpg?hmac=7epGozH4QjToGaBf_xb2HbFTXoV5o8n_cYzB7I4lt6g", titulo: "Yellow Tree", descripcion: "Lorem Ipsum is simply dummy text of the printing and typesetting industry." },
+    { 
+      img: "https://fastly.picsum.photos/id/1/5000/3333.jpg?hmac=Asv2DU3rA_5D1xSe22xZK47WEAN0wjWeFOhzd13ujW4", titulo: "Work", descripcion: "Lorem Ipsum is simply dummy text of the printing and typesetting industry." },
+
+  ]
+
+  return (
+    <>
+     <div className="w-100">
+  <Navbar />
+</div>
+
+<div className="container">
+  <div className="bg-body-secondary p-4 mt-2 shadow-sm mb-4">
+    <Jumbotron />
+  </div>
+
+  <div className="row justify-content-center">
+    {paisajes.map((item, index) => (
+      <div className="col-md-3" key={index}>
+        <Card img={item.img} titulo={item.titulo} descripcion={item.descripcion} />
       </div>
-	  
-	  
-  </>
+    ))}
+  </div>
+</div>
+    </>
   );
 };
 
