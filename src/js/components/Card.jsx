b51a64abe462583f8af4c0f4b6c807039dee1e1a
Lucas Matias Calvo <lucascalvo2903@gmail.com>
2025-10-14 01:00:44 +0000

    ya casi listo primera parte

--- src/js/components/Card.jsx
+++ src/js/components/Card.jsx
@@ -10,9 +10,9 @@ const Card = () => {
         <div className="col-md-3">
           <div className="card" style={{ width: "18rem" }}>
             <img src="https://fastly.picsum.photos/id/29/4000/2670.jpg?hmac=rCbRAl24FzrSzwlR5tL-Aqzyu5tX_PA95VJtnUXegGU" className="card-img-top" alt="..." />
-            <div className="card-body">
-              <h5 className="card-title">Carta 1</h5>
-              <p className="card-text">Texto de ejemplo de la carta 1.</p>
+            <div className="card-body text-center">
+              <h5 className="card-title">Carta</h5>
+              <p className="card-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. </p>
               <a href="#" className="btn btn-primary">Botón</a>
             </div>
           </div>
@@ -23,9 +23,9 @@ const Card = () => {
         <div className="col-md-3">
           <div className="card" style={{ width: "18rem" }}>
             <img src="https://fastly.picsum.photos/id/16/2500/1667.jpg?hmac=uAkZwYc5phCRNFTrV_prJ_0rP0EdwJaZ4ctje2bY7aE" className="card-img-top" alt="..." />
-            <div className="card-body">
-              <h5 className="card-title">Carta 2</h5>
-              <p className="card-text">Texto de ejemplo de la carta 2.</p>
+            <div className="card-body text-center">
+              <h5 className="card-title">Carta</h5>
+              <p className="card-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. </p>
               <a href="#" className="btn btn-primary">Botón</a>
             </div>
           </div>
@@ -35,9 +35,9 @@ const Card = () => {
         <div className="col-md-3">
           <div className="card" style={{ width: "18rem" }}>
             <img src="https://fastly.picsum.photos/id/19/2500/1667.jpg?hmac=7epGozH4QjToGaBf_xb2HbFTXoV5o8n_cYzB7I4lt6g" className="card-img-top" alt="..." />
-            <div className="card-body">
-              <h5 className="card-title">Carta 3</h5>
-              <p className="card-text">Texto de ejemplo de la carta 3.</p>
+            <div className="card-body text-center">
+              <h5 className="card-title">Carta</h5>
+              <p className="card-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. </p>
               <a href="#" className="btn btn-primary">Botón</a>
             </div>
           </div>
@@ -46,9 +46,9 @@ const Card = () => {
         <div className="col-md-3">
           <div className="card" style={{ width: "18rem" }}>
             <img src="https://fastly.picsum.photos/id/1/5000/3333.jpg?hmac=Asv2DU3rA_5D1xSe22xZK47WEAN0wjWeFOhzd13ujW4" className="card-img-top" alt="..." />
-            <div className="card-body">
-              <h5 className="card-title">Carta 3</h5>
-              <p className="card-text">Texto de ejemplo de la carta 3.</p>
+           <div className="card-body text-center">
+              <h5 className="card-title">Carta</h5>
+              <p className="card-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. </p>
               <a href="#" className="btn btn-primary">Botón</a>
             </div>
           </div>
--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -4,31 +4,29 @@ import React from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 import Navbar from "./Navbar.jsx";
 import Card from "./Card.jsx";
-import Cardmaster from "./Cardmaster.jsx"
+import Jumbotron from "./Jumbotron.jsx"
 
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<Navbar/>
-            <Cardmaster/>
+  return (
+<>
+      <div className="w-100">
+        <Navbar />
+      </div>
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-			<Card/>
-		</div>
-	);
+      <div className="container">
+		 <div className="bg-body-secondary p-4 mt-2 shadow-sm">
+        <Jumbotron />
+		 </div>
+		 </div>
+	<div className="container">
+        <Card />
+      </div>
+	  
+	  
+  </>
+  );
 };
 
 export default Home;
